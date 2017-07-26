bd74470e00aabe7762653b68caaa721362d053f6
Ningxin Hu <ningxin.hu@intel.com>
2017-07-26 14:33:34 +0800

    Update to aeb41163ba6d98ad424bfe6cfbd5fe76d740c028

--- search/pages_11.js
+++ search/pages_11.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['todo_20list',['Todo List',['../todo.html',1,'']]],
+  ['template_20matching',['Template Matching',['../tutorial_js_template_matching.html',1,'tutorial_js_table_of_contents_imgproc']]],
   ['template_20matching',['Template Matching',['../tutorial_py_template_matching.html',1,'tutorial_py_table_of_contents_imgproc']]],
   ['trackbar_20as_20the_20color_20palette',['Trackbar as the Color Palette',['../tutorial_py_trackbar.html',1,'tutorial_py_table_of_contents_gui']]],
   ['the_20core_20functionality_20_28core_20module_29',['The Core Functionality (core module)',['../tutorial_table_of_content_core.html',1,'tutorial_root']]],
